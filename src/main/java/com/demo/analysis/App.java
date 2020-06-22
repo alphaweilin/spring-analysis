23a4926a62e796bdc05f37e9db41a1af1f92261d
Wei Lin <whlw@cn.ibm.com>
2020-06-22 14:53:58 +0800

    update

--- src/main/java/com/demo/analysis/App.java
+++ src/main/java/com/demo/analysis/App.java
@@ -9,12 +9,23 @@ import com.demo.analysis.service.UserService;
 public class App {
 	public static void main(String[] args) {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+		beanFactoryTest(ac);
+	}
+	
+	public static void beanFactoryTest(ApplicationContext ac) {
 //		System.out.println(ac.getBean("serviceBean"));
 		UserService serviceBean = (UserService) ac.getBean("userService");
 		serviceBean.login();
+	}
+	
+	public static void iocTest(ApplicationContext ac) {
+		UserController controllerBean = (UserController) ac.getBean("userController");
+		controllerBean.login();
+	}
+	
+	public static void aopTest(ApplicationContext ac) {
+		UserService serviceBean = (UserService) ac.getBean("userService");
+		serviceBean.login();
 		serviceBean.register();
-		
-//		UserController controllerBean = (UserController) ac.getBean("userController");
-//		controllerBean.login();
 	}
 }
